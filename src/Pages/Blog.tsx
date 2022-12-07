b9665ea57fb2a94e39680886acd80b307994d03f
parsobsri <parsobsri@kosoybad-MacBook-Pro.local>
2022-12-08 05:11:17 +0700

    up

--- src/Pages/Blog.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState, FC } from "react";
-import Navbar from "../component/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
-import Footer from "../component/Footer";
-import Card from "../component/Card";
-
