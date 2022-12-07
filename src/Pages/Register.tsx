2d3cfd67c1935ed9162a8d3128454c3dca9a8b73
parsobsri <parsobsri@kosoybad-MacBook-Pro.local>
2022-12-07 21:12:29 +0700

    today

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -57,12 +57,16 @@ const Register = () => {
     }
   };
 
+  const registerClick = async () =>{
+    navigate("/Login")
+  }
+
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            kuy oak
+            Register
           </h2>
         </div>
         <div className="-space-y-px rounded-md shadow-sm">
@@ -101,6 +105,7 @@ const Register = () => {
             </span>
             Sign in
           </button>
+          <button type="submit" onClick={registerClick}> Login </button>
         </div>
       </div>
     </div>
--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -20,18 +20,18 @@ const Footer: FC = () => {
               <i className="fa fa-asterisk"></i>
             </div>
             <div className="col-lg-12">
-              อาคารวิทยาลัยนวัตกรรมสื่อสารสังคม (ตึกริมน้ำ) &nbsp;
-              <span className="nextlineSM">ฝ่ายวิชาการและวิจัย ชั้น 15</span>
+            นาย ณัฐชนันท์  &nbsp;
+              <span className="nextlineSM">เจียรสุวรรณ  </span>
             </div>
             <div className="col-lg-12">
-              <u>ติดต่อในวันเวลาราชการ</u> &nbsp;
+              นาย ศุภกฤต  &nbsp;
               <span className="nextlineSM">
-                วันจันทร์ถึงศุกร์ เวลา 08:30น. - 16:30น.
+              เผือกประพันธุ์  
               </span>
             </div>
             <div className="col-lg-12">
               <span className="nextlineSM">
-                (ยกเว้นวันหยุดนักขัตฤกษ์) โทร 02-649-5000 ต่อ 11268
+              นาย สมชัย ตันติชาญชัยกุล 
               </span>
             </div>
           </div>
